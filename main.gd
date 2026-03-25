919b6e03e6f89a01141bb9b3d7493e3fb8450c8d
UrtaroPrograms <urtasunltrabajo@gmail.com>
2026-03-25 18:17:46 -0300

    La escena principal del juego

---
+++ main.gd
@@ -0,0 +1,48 @@
+extends Node
+@export var escena_mob : PackedScene
+var puntos
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float):
+	pass
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	
+func new_game():
+	puntos = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+func _on_mob_timer_timeout():
+	var mob = escena_mob.instantiate()
+	
+	var mob_ubicacion_spawn = $MobPath/MobSpawnLocation
+	mob_ubicacion_spawn.progress_ratio = randf()
+	
+	mob.position = mob_ubicacion_spawn.position
+	
+	var direccion = mob_ubicacion_spawn.rotation + PI/2
+	
+	direccion += randf_range(-PI/4, PI/4)
+	mob.rotation = direccion
+	
+	var velocidad = Vector2(randf_range(150.0,250.0), 0.0)
+	mob.linear_velocity = velocidad.rotated(direccion)
+	
+	add_child(mob)
+
+func _on_score_timer_timeout():
+	puntos += 1
+
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
